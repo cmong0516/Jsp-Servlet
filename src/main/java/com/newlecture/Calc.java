34e73bcbe092d29e54053f7b5877f48ff121449c
cmong0516 <cmong0516@gmail.com>
2022-03-13 05:08:45 +0900

    2022.3.13 application

--- src/main/java/com/newlecture/Calc.java
+++ src/main/java/com/newlecture/Calc.java
@@ -1,36 +1,37 @@
 package com.newlecture;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/calc")
+public class Calc extends HttpServlet {
 
-@WebServlet("/notice-reg")
-public class NoticeReg extends HttpServlet {
-	
-	
-	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
 		// UTF-8로 보냈어!!
 		response.setCharacterEncoding("UTF-8");
 		// UTF-8로 읽어!!
 		response.setContentType("text/html; charset=UTF-8");
-		// UTF-8로 받아!!
-		
-		PrintWriter out = response.getWriter();
-		
-		String title = request.getParameter("title");
-		String content = request.getParameter("content");
+		String x_ = request.getParameter("number");
+		String y_ = request.getParameter("number2");
+		String op = request.getParameter("button");
 
-		out.println(title);
-		out.println(content);
+		int x = 0;
+		int y = 0;
+
+		if (!x_.equals(""))
+			x = Integer.parseInt(x_);
+		if (!y_.equals(""))
+			y = Integer.parseInt(y_);
+
+		int result = x + y;
+
+		response.getWriter().printf("result is %d , %s\n", result, op);
 	}
-
 
 }
